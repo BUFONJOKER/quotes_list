fe910e917204b95367d0f2f8ea09a425f0ffb112
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-26 21:21:32 +0500

    flutter quotes list app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.blueGrey[600],
+          title: const Center(
+            child: Text("Quotes List"),
+
+          ),
+      ),
+    );
+  }
+}
