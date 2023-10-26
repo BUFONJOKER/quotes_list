fd262d51134b2aeb57bcd9af7f42cceef053c3cf
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-26 21:38:48 +0500

    list created and map function

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var quoteList = [
+    "Believe you can and you're halfway there. -Theodore Roosevelt",
+    "The only way to do great work is to love what you do. -Steve Jobs",
+    "The future depends on what you do today. -Mahatma Gandhi",
+    "Don't watch the clock; do what it does. Keep going. -Sam Levinson",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
           backgroundColor: Colors.blueGrey[600],
-          title: const Center(
-            child: Text("Quotes List"),
+          title: const Text('Awesome Quotes'),
+          centerTitle: true,
+      ),
+      body: Column(
+        children: quoteList.map((quote) => Text(quote)).toList(),
 
-          ),
       ),
     );
   }
